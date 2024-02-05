f5ccd9e13af19a772962a7bc9f5da435b4985f83
A-K-2001 <kumaranilstps017@gmail.com>
2024-02-05 09:32:03 +0530

    migration to prisma and sqlite

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import fastify from 'fastify';
 import { MongoClient, ObjectId } from 'mongodb';
-import dotenv from 'dotenv';
+import dotenv, { parse } from 'dotenv';
 import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const app = fastify();
+const prismadb = new PrismaClient();
 
-let db;
 
-MongoClient.connect('mongodb://localhost:27017/farmako')
-    .then((client) => {
-        db = client.db();
+const start = async () => {
+    try {
         app.listen(process.env.PORT, () => {
             console.log('Running at port:', process.env.PORT);
         });
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+    } catch (error) {
+        console.log('Error in starting server:', error);
+    }
+};
+start();
+// console.log(prismadb,"PP");
 
 function verifyToken(req, res, next) {
     const token = req.headers['authorization'];
@@ -53,7 +55,7 @@ const userSchema = {
 
 app.get('/user', { preHandler: verifyToken, schema: userSchema }, async (req, res) => {
     try {
-        const users = await db.collection('user').find().toArray();
+        const users = await prismadb.user.findMany();
         // console.log(users);
         res.code(200).send(users);
     } catch (error) {
@@ -63,12 +65,22 @@ app.get('/user', { preHandler: verifyToken, schema: userSchema }, async (req, re
 
 app.get('/user/:id', { preHandler: verifyToken }, async (req, res) => {
     try {
-        if (ObjectId.isValid(req.params.id)) {
-            const user = await db.collection('user').findOne({ _id: new ObjectId(req.params.id) });
+        const userId = parseInt(req.params.id);
+        if (isNaN(userId)) {
+            res.code(400).send({ error: 'not valid id' });
+            return
+        }
+        const user = await prismadb.user.findUnique({
+            where: {
+                id: userId,
+            },
+        });
+        if (user) {
             res.code(200).send(user);
         } else {
-            res.code(500).send({ error: 'not valid id' });
+            res.code(404).send({ error: 'not found' });
         }
+
     } catch (error) {
         res.code(500).send({ error: 'not found' });
     }
@@ -78,7 +90,13 @@ app.post('/user', { preHandler: verifyToken }, async (req, res) => {
     const user = req.body;
     if (user.email && user.username) {
         try {
-            const result = await db.collection('user').insertOne(user);
+            const result = await prismadb.user.create({
+                data: {
+                    username: user.username,
+                    email: user.email,
+                    role: user.role,
+                },
+            });
             res.code(200).send(result);
         } catch (error) {
             res.code(500).send({ err: 'error in saving document' });
@@ -90,28 +108,43 @@ app.post('/user', { preHandler: verifyToken }, async (req, res) => {
 
 app.delete('/user/:id', { preHandler: verifyToken }, async (req, res) => {
     try {
-        if (ObjectId.isValid(req.params.id)) {
-            const result = await db.collection('user').deleteOne({ _id: new ObjectId(req.params.id) });
-            res.code(200).send(result);
-        } else {
-            res.code(500).send({ error: 'not valid id' });
+        const userId = parseInt(req.params.id);
+
+        if (isNaN(userId)) {
+            res.code(400).send({ error: 'Invalid user id' });
+            return;
         }
+
+        const result = await prismadb.user.delete({
+            where: { id: userId },
+        });
+
+        res.code(200).send(result);
     } catch (error) {
-        res.code(500).send({ error: 'not deleted' });
+        console.error(error);
+        res.code(500).send({ error: 'User not deleted' });
     }
 });
 
 app.patch('/user/:id', { preHandler: verifyToken }, async (req, res) => {
+    const userId = parseInt(req.params.id);
     const newUser = req.body;
+  
+    if (isNaN(userId)) {
+      res.code(400).send({ error: 'Invalid user id' });
+      return;
+    }
+  
     try {
-        if (ObjectId.isValid(req.params.id)) {
-            const result = await db.collection('user').updateOne({ _id: new ObjectId(req.params.id) }, { $set: newUser });
-            res.code(200).send(result);
-        } else {
-            res.code(500).send({ error: 'not valid id' });
-        }
+      const updatedUser = await prismadb.user.update({
+        where: { id: userId },
+        data: newUser,
+      });
+  
+      res.code(200).send(updatedUser);
     } catch (error) {
-        res.code(500).send({ error: 'not updated' });
+      console.error(error);
+      res.code(500).send({ error: 'User not updated' });
     }
 });
 
@@ -119,11 +152,21 @@ app.post('/login', async (req, res) => {
     const user = req.body;
     if (user.username) {
         try {
-            const result = await db.collection('user').findOne({ username: user.username });
-            jwt.sign({ username: result.user, role: result.role }, process.env.JWT_SCRT, { expiresIn: 3000 }, (err, token) => {
+            const result = await prismadb.user.findUnique({
+                where: {
+                    username: user.username,
+                },
+            });
+            console.log(result);
+            if(!result) {
+                res.code(404).send({ err: 'user not found' });
+                return;
+            }
+            jwt.sign({ username: result.username, role: result.role }, process.env.JWT_SCRT, { expiresIn: 3000 }, (err, token) => {
                 res.code(201).send({ token });
             });
         } catch (error) {
+            console.log(error);
             res.code(500).send({ err: 'error in login' });
         }
     } else {
---
+++ prisma/migrations/20240205031844_dev1/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "role" TEXT NOT NULL
+);
